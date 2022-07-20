b960b47cb04f3b6936fadcb6b43bb00ed5e55c1d
Divakar223 <divakar.devaraji@agilisium.com>
2022-07-20 12:29:38 +0530

    createPassword

--- co-creator-ui/components/ContactPage/ContactPage.tsx
+++ co-creator-ui/components/ContactPage/ContactPage.tsx
@@ -2,10 +2,14 @@ import React, { useState } from 'react'
 import TimezoneSelect, { allTimezones } from 'react-timezone-select'
 import PhoneInput from 'react-phone-input-2'
 import 'react-phone-input-2/lib/style.css'
+import DatePicker from 'react-DatePicker'
+import 'react-datepicker/dist/react-datepicker.css'
 
 export default function ContactPage() {
   const [selectedTimezone, setSelectedTimezone] = useState('')
+  const [selectedDate, setSelectedDate] = useState(null)
   const [value, setValue] = useState('sdkf')
+  const [time, setTime] = useState('10:00')
   return (
     <div>
       <div className="w-3/6 mx-auto">
@@ -26,6 +30,14 @@ export default function ContactPage() {
         </div>
 
         <p>When would you like to connect?</p>
+        <div>
+          <DatePicker
+            selected={selectedDate}
+            onChange={(date: any) => setSelectedDate(date)}
+            placeholderText={'Today'}
+            className="border border-[1] border-gray-300 py-1 w-full my-2 rounded"
+          />
+        </div>
 
         <div>
           <TimezoneSelect
@@ -38,7 +50,7 @@ export default function ContactPage() {
             }}
           />
         </div>
-        <div>{/* <TimePicker /> */} Time</div>
+        <div>{/* <TimePicker /> */}</div>
         <div className="text-center">
           <button className="bg-blue-500 text-white py-2 rounded px-10 mt-3">
             {' '}
