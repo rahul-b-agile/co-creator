a0944b1b6d37c429a4b92c3769e7375550d08f05
AjayShanmugasundram <ajay.shanmugasundram@agilisium.com>
2022-07-20 15:19:37 +0530

    as

--- co-creator-ui/components/Functions/function.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function functionCall(e: any, data: any, particularData: any) {
-  console.log('function call Happpens', e.target.value, data, particularData)
-}
--- co-creator-ui/pages/context.tsx
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-
-export const Context1 = React.createContext(null)
